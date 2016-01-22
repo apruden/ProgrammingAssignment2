59b5b76b561b42120b2bec385da7085c94f7d1eb
Alex Prudencio <alex.prudencio@gmail.com>
2016-01-22 14:03:33 -0500

    submission

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,46 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+# Creates a cacheable matrix object wrapping a matrix x. The cacheable matrix
+# is used to cache the inverse of x calculated with cacheSolve.
+#
+# Args:
+#   x: matrix to be wrapped in a cacheable matrix object.
+#
+# Return:
+#   cacheable matrix object.
+#
 makeCacheMatrix <- function(x = matrix()) {
+    cached <- NULL
+    set <- function(data) {
+        x <<- data
+        cached <<- NULL
+    }
 
+    get <- function() x
+    setinv <- function(inv) cached <<- inv
+    getinv <- function() cached
+
+    list(set=set, get=get, setinv=setinv, getinv = getinv)
 }
 
 
-## Write a short comment describing this function
-
+# Calculates the inverse of a cacheable matrix object created with makeCacheMatrix
+# function. The inverse is calculated once (the first function call). The result is
+# cached and returned on next calls.
+#
+# Args:
+#   x: cacheable matrix object created with makeCacheMatrix function.
+#
+# Return:
+#   matrix inverse or x.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    inv <- x$getinv()
+
+    if (!is.null(inv)) {
+        message('Return cached result')
+        return(inv)
+    }
+
+    data <- x$get()
+    inv <- solve(data, ...)
+    x$setinv(inv)
+    inv
 }
